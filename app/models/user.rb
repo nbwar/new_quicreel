77d954a039449478b4c78216b42f663525c71d19
Nicholas Wargnier <nwargnier@gmail.com>
2013-08-30 11:17:56 -0700

    user unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates_presence_of :username, :email
-  # validates_presence_of :password_confirmation, :if => :password_changed?
   validates_uniqueness_of :username, :email
   validates_format_of :email, :with => VALID_EMAIL_REGEX
   validates_format_of :username, :with => /\A\w+\z/, :allow_nil => true, :message => 'can only contain letters, numbers, hyphens, or underscores'
@@ -17,6 +16,9 @@ class User < ActiveRecord::Base
   # has_many :followers, :through => :user_connections_as_followee, :source => :follower, :conditions => ['accepted_at IS NOT NULL']
   # has_many :following, :through => :user_connections_as_follower, :source => :followee, :conditions => ['accepted_at IS NOT NULL']
 
+  before_save :downcase_email
+  before_create :create_remember_token
+
   def self.new_remember_token
     SecureRandom.urlsafe_base64
   end
@@ -25,12 +27,12 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
-  # def password_changed?
-  #   !password.nil?
-  # end
-
   private
     def create_remember_token
       self.remember_token = User.encrypt(User.new_remember_token)
+    end
+
+    def downcase_email
+      self.email = email.downcase
     end
 end
